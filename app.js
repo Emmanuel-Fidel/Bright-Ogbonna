ec5714ee876ba7f5084311abbf21e305aefd6c63
Emmanuel-Fidel <133176199+Emmanuel-Fidel@users.noreply.github.com>
2023-05-12 09:53:27 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 let menu = document.querySelector("#menu-bar");
-let navbar = document.querySelector(".navbar");
+let navbar = document.querySelector(".navbar1");
 
 menu.onclick = () =>{
 
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 }
+
+
+
+
+
+
+
+
 
 
 
